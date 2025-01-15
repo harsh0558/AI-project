b86c1fc36712eecc0a5762d0d92844721e7f661d
Harsh Maghnani <hmaghnani6@gmail.com>
2025-01-15 05:16:58 +0500

    api fetch completed

---
+++ script.js
@@ -0,0 +1,47 @@
+const key = "AIzaSyDC7ObOJhCGZojTvWkSvQxPr3fDt_7heUU";
+const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${key}`;
+
+let btn = document.querySelector("#btn");
+let prompt = document.querySelector("#prompt");
+let text_box = document.querySelector("#answer")
+let userMessage;
+btn.onclick = async(event) => {
+    event.preventDefault();
+    userMessage = prompt.value;
+    let answer = await generateAPIResponse();
+    text_box.innerText = answer;
+    
+}
+
+const generateAPIResponse = async () => {
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                contents: [{
+                    role: "user",
+                    parts: [{ text: userMessage }]
+                }]
+            })
+        });
+        
+
+        const data = await response.json();
+        console.log(data);
+        let answer = data.candidates[0].content.parts[0].text;
+        console.log(data.candidates[0].content.parts[0].text);
+        return answer;
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
+
+
+
+
+
+
