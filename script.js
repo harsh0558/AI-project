dcc186c0c6ff139f9232a7ee6f28a3f74f1828c7
Harsh Maghnani <hmaghnani6@gmail.com>
2025-01-15 20:16:11 +0500

    ai priject completed

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const key = "AIzaSyDC7ObOJhCGZojTvWkSvQxPr3fDt_7heUU";
 const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${key}`;
 
-let btn = document.querySelector("#btn");
-let prompt = document.querySelector("#prompt");
-let text_box = document.querySelector("#answer")
 let userMessage;
-
+let btn = document.querySelector("#send_icon");
+let prompt = document.querySelector("#prompt");
+let trigger = 0;
 
 const generateAPIResponse = async () => {
     try {
@@ -21,10 +20,8 @@ const generateAPIResponse = async () => {
                 }]
             })
         });
-        
 
         const data = await response.json();
-        
         let answer = data.candidates[0].content.parts[0].text;
         return answer;
 
@@ -33,16 +30,49 @@ const generateAPIResponse = async () => {
     }
 };
 
-btn.onclick = async(event) => {
-    event.preventDefault();
+const sendMessage = async() => {
+
     userMessage = prompt.value;
+
+    if (trigger === 0) {
+        let element = document.querySelector("#logo");
+        element.remove(); 
+        trigger = 1; 
+    }
+
+    let messageContainerUser = document.createElement("div");
+    messageContainerUser.classList.add("user");
+    const messageTextUser = document.createTextNode(userMessage);
+    messageContainerUser.appendChild(messageTextUser);
+    document.querySelector("#chat").appendChild(messageContainerUser);
+
+    prompt.value = '';
+
     let answer = await generateAPIResponse();
-    text_box.innerText = answer;
+
+    let messageContainerBot = document.createElement("div");
+    messageContainerBot.classList.add("bot");
+
+    let logoImg = document.createElement("img");
+    logoImg.src = "logo.webp"; 
+    logoImg.classList.add("bot-logo");
+
+    const messageTextBot = document.createTextNode(answer);
     
+    messageContainerBot.appendChild(logoImg);
+    messageContainerBot.appendChild(messageTextBot);
+    
+    document.querySelector("#chat").appendChild(messageContainerBot);
 }
 
+btn.onclick = (event) => {
+    event.preventDefault();
+    sendMessage();
+}
 
-
-
-
-
+prompt.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        sendMessage();
+    }
+});
